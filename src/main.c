98ed4fca217016539f2d2b412a1395fd901609cc
Your Name <you@example.com>
2022-03-12 19:50:59 +0300

    add free env

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include "minishell.h"
 #include "stdio.h"
 
+void	free_env(t_env *env)
+{
+	t_env	*tmp;
+
+	while (env)
+	{
+		free(env->data);
+		tmp = env->next;
+		free(env);
+		env = tmp;
+	}
+}
+
 int	main(int argc, char **argv, char **envm)
 {
 	t_env	*env;
@@ -11,5 +24,6 @@ int	main(int argc, char **argv, char **envm)
 		printf("%s\n", env->data);
 		env = env->next;
 	}
+	free_env(env);
 	return (0);
 }
